3fee9b18e59ec999689198663f0c81f00932ed66
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-11 16:45:09 +0200

    Update generate_data.jl

--- src/generate_data.jl
+++ src/generate_data.jl
@@ -6,8 +6,24 @@ export TimeSeries
 mutable struct TimeSeries
 
    time   :: Vector{Float64}
-   values :: Array{Float64,2}
+   values :: Array{Union{Float64,Missing},2}
 
+end
+
+export StateSpace
+
+struct StateSpace
+
+    dt_integration :: Float64
+    dt_states      :: Int
+    dt_obs         :: Int
+    params         :: Vector{Float64}
+    var_obs        :: Vector{Int64}
+    nb_loop_train  :: Int
+    nb_loop_test   :: Int
+    sigma2_catalog :: Float64
+    sigma2_obs     :: Float64
+    
 end
 
 export Catalog
@@ -16,22 +32,6 @@ mutable struct Catalog
 
    analogs    :: Array{Float64,2}
    successors :: Array{Float64,2}
-   sources    :: Array{Float64,2}
+   sources    :: StateSpace
 
 end
-
-export StateSpace
-
-struct StateSpace
-
-    dt_states      :: Float64
-    dt_obs         :: Float64
-    params         :: Vector{Float64}
-    var_obs        :: Vector{Int64}
-    nb_loop_train  :: Int64
-    nb_loop_test   :: Int64
-    sigma2_catalog :: Float64
-    sigma2_obs     :: Float64
-    
-end
-
