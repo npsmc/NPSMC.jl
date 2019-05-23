185a78c48d3134e2bf9ed6a1f9c31aafda60daf8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-23 14:23:41 +0200

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -64,20 +64,22 @@ function inv_using_SVD(Mat, eigvalMax)
     F = svd(Mat; full = true)
     eigval = cumsum(F.S) ./ sum(F.S)
     # search the optimal number of eigen values
-    i_cut = findfirst(eigval .>= eigvalMax)
+    icut = findfirst(eigval .>= eigvalMax)
 
-    U_1 = @view F.U[1:i_cut,1:i_cut]
-    U_3 = @view F.U[min(i_cut+1,end):end,1:i_cut]
-    
-    V_1 = @view F.Vt'[1:i_cut,1:i_cut]
-    V_3 = @view F.Vt'[min(i_cut+1,end):end,1:i_cut]
+    U_1 = @view F.U[1:icut,1:icut]
+    V_1 = @view F.Vt'[1:icut,1:icut]
+    tmp1 = (V_1 ./ F.S[1:icut]') * U_1'
 
-    tmp1 = (V_1 ./ F.S[1:i_cut]') * U_1'
-    tmp2 = (V_1 ./ F.S[1:i_cut]') * U_3'
-    tmp3 = (V_3 ./ F.S[1:i_cut]') * U_1'
-    tmp4 = (V_3 ./ F.S[1:i_cut]') * U_3'
+    if icut+1 > length(eigval)
+       tmp1
+    else
+       U_3 = @view F.U[icut+1:end,1:icut]
+       V_3 = @view F.Vt'[icut+1:end,1:icut]
+       tmp2 = (V_1 ./ F.S[1:icut]') * U_3'
+       tmp3 = (V_3 ./ F.S[1:icut]') * U_1'
+       tmp4 = (V_3 ./ F.S[1:icut]') * U_3'
+       vcat(hcat(tmp1,tmp2),hcat(tmp3,tmp4))
+    end
 
-    vcat(hcat(tmp1,tmp2),hcat(tmp3,tmp4))
-    
 end
     
