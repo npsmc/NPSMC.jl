ade0dad39d0a6c644f37015b9e73a07ece7f7d02
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-24 10:38:19 +0200

    Update generate_data.jl

--- src/generate_data.jl
+++ src/generate_data.jl
@@ -11,54 +11,49 @@ from StateSpace generate:
  - partial/noisy observations (yo)
  - catalog
 """
-function generate_data( ssm :: StateSpaceModel )
+function generate_data( ssm :: StateSpaceModel, u0 :: Vector{Float64} )
 
     @assert ssm.dt_states < ssm.dt_obs
     # @error " ssm.dt_obs must be bigger than ssm.dt_states"
     @assert mod(ssm.dt_obs,ssm.dt_states) == 0.0
     # @error " ssm.dt_obs must be a multiple of ssm.dt_states "
     
-    # 5 time steps (to be in the attractor space)       
-    x0    = [8.0;0.0;30.0]
-    tspan = (0.0,5.0)
-    p     = [10.0,28.0,8/3]
-    prob  = ODEProblem(lorenz63, x0, tspan, p)
-    
-    x0    = last(solve(prob, reltol=1e-6, save_everystep=false))
     tspan = (0.0,ssm.nb_loop_test)
-    prob  = ODEProblem(lorenz63, x0, tspan, p)
+    prob  = ODEProblem(ssm.model, u0, tspan, ssm.params)
     
     # generSate true state (xt)
     sol = solve(prob,reltol=1e-6,saveat=ssm.dt_states*ssm.dt_integration)
-    xt  = TimeSeries(sol.t, vcat(sol.u'...))
+    xt  = TimeSeries(sol.t, hcat(sol.u...))
     
     # generate  partial/noisy observations (yo)
-    d       = MvNormal(ssm.sigma2_obs .* Matrix(I,3,3))
-    nvalues = length(xt.time)
-    eps     = rand(d, nvalues)
+    nt   = xt.nt
+    nv   = xt.nv
+    d    = MvNormal(ssm.sigma2_obs .* Matrix(I,nv,nv))
     
-    yo   = TimeSeries( xt.time, xt.values .* NaN)
-    step = ssm.dt_obs ÷ ssm.dt_states
-    nt   = length(xt.time)
-    for j in ssm.var_obs
-        for i in 1:step:nt
-            yo.values[i,j] = xt.values[i,j] + eps[j,i]
+    yo         = TimeSeries( xt.time, xt.values .* NaN)
+    step       = ssm.dt_obs ÷ ssm.dt_states
+    nt         = length(xt.time)
+    ε          = rand(d, nt)
+    @show size(yo.values), size(xt.values), size(ε)
+    
+    for j in 1:step:nt
+        for i in ssm.var_obs
+            yo.values[i,j] = xt.values[i,j] + ε[i,j]
         end
     end
     
     # generate catalog
-    x0 = last(sol)
+    u0    = last(sol)
     tspan = (0.0,ssm.nb_loop_train)
-    prob = ODEProblem(lorenz63, x0, tspan, p)
-    sol = solve(prob,reltol=1e-6,saveat=ssm.dt_integration)
-    n = length(sol.t)
+    prob  = ODEProblem(ssm.model, u0, tspan, ssm.params)
+    sol   = solve(prob,reltol=1e-6,saveat=ssm.dt_integration)
+    n     = length(sol.t)
     catalog_tmp = hcat(sol.u...) 
     if ssm.sigma2_catalog > 0
-        μ   = [0.,0.0,.0]
-        σ   = ssm.sigma2_catalog .* Matrix(I,3,3)
+        μ   = zeros(Float64,nv)
+        σ   = ssm.sigma2_catalog .* Matrix(I,nv,nv)
         d   = MvNormal(μ, σ)
-        eta = rand(d, n)
-        catalog_tmp .+= eta
+        catalog_tmp .+= rand(d, n)
     end
 
     xt, yo, Catalog( catalog_tmp, ssm )
