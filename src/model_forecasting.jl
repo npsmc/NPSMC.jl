06edb90af72b922e4f567503cb73f90c32354f59
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-24 10:38:15 +0200

    Update model_forecasting.jl

--- src/model_forecasting.jl
+++ src/model_forecasting.jl
@@ -6,10 +6,11 @@ struct ModelForecasting <: AbstractForecasting
 
     dt     :: Float64
     params :: Vector{Float64}
+    model  :: Function
 
     function ModelForecasting( ssm :: StateSpaceModel )
 
-        new( ssm.dt_integration, ssm.params )
+        new( ssm.dt_integration, ssm.params, ssm.model )
 
     end
 
@@ -22,15 +23,14 @@ end
 function ( m :: ModelForecasting )( x :: Array{Float64, 2})
 
     np, nv = size(x)
-    @assert nv == 3 
-    p     = m.params
-    tspan = (0.0, m.dt)
-    x0    = [8.0;0.0;30.0]
+    p      = m.params
+    tspan  = (0.0, m.dt)
+    u0     = zeros(Float64,nv)
 
-    prob  = ODEProblem( lorenz63, x0, tspan, p)
+    prob   = ODEProblem( ssm.model, u0, tspan, p)
 
     function prob_func( prob, i, repeat)
-        prob.u0 .= x[i,:]
+        prob.u0 .= x[:, i]
         prob
     end
 
@@ -40,7 +40,7 @@ function ( m :: ModelForecasting )( x :: Array{Float64, 2})
 
     sol = [last(sim[i].u) for i in 1:np]
 
-    xf = vcat(sol'...)
+    xf = hcat(sol...)
 
     return xf, xf
             
