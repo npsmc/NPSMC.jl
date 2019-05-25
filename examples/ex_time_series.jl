c24f5e0552e6327525e1b3daf77736f7c4e3f0b6
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-25 22:28:55 +0200

    Update ex_time_series.jl

--- examples/ex_time_series.jl
+++ examples/ex_time_series.jl
@@ -6,24 +6,21 @@
 #       extension: .jl
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.1.1
+#       jupytext_version: 1.1.3
 #   kernelspec:
-#     display_name: Julia 1.1.0
+#     display_name: Julia 1.1.1
 #     language: julia
 #     name: julia-1.1
 # ---
 
-include("../src/models.jl")
-include("../src/generate_data.jl")
+using NPSMC
 
 nt, nv = 10, 3
 xt = TimeSeries{Float64}(nt, nv)
 
 using Random
 time = collect(0:10.0)
-values = rand( nt, nv)
+values = [rand(nv) for i = 1:nt]
 yo = TimeSeries(time, values)
 
 typeof(yo.time), typeof(yo.values)
-
-
