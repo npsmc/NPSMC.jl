bbb0fe48cc18cf2fcf4395e64edb530fa2f8aca8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-25 21:29:24 +0200

    Create test_model_forecasting.jl

---
+++ test/test_model_forecasting.jl
@@ -0,0 +1,40 @@
+@testset " Classic model forecasting " begin
+
+import NPSMC: normalise!, sample_discrete
+import DifferentialEquations: ODEProblem, solve
+
+σ = 10.0
+ρ = 28.0
+β = 8.0/3
+
+dt_integration = 0.01
+dt_states      = 1 
+dt_obs         = 8 
+parameters     = [σ, ρ, β]
+var_obs        = [1]
+nb_loop_train  = 10^2 
+nb_loop_test   = 10
+sigma2_catalog = 0.0
+sigma2_obs     = 2.0
+
+ssm = StateSpaceModel( lorenz63,
+                       dt_integration, dt_states, dt_obs, 
+                       parameters, var_obs,
+                       nb_loop_train, nb_loop_test,
+                       sigma2_catalog, sigma2_obs )
+
+# compute u0 to be in the attractor space
+u0    = [8.0;0.0;30.0]
+tspan = (0.0,5.0)
+prob  = ODEProblem(ssm.model, u0, tspan, parameters)
+u0    = last(solve(prob, reltol=1e-6, save_everystep=false))
+
+xt, yo, catalog = generate_data( ssm, u0 )
+
+da = DataAssimilation( ssm, :AnEnKS, 100, xt, ssm.sigma2_obs )
+
+@time x̂ = data_assimilation(yo, da);
+
+@test RMSE(xt, x̂) < 1.0
+
+end
