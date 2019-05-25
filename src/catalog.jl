36d9efc7e75288d5a4cd0a648b47185dfef72a17
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-25 20:04:07 +0200

    Update catalog.jl

--- src/catalog.jl
+++ src/catalog.jl
@@ -4,20 +4,19 @@ mutable struct Catalog{T}
 
    nt         :: Int64
    nv         :: Int64
-   data       :: Vector{Array{T,1}}
-   analogs    :: Vector{Array{T,1}}
-   successors :: Vector{Array{T,1}}
+   data       :: Array{T,2}
+   analogs    :: Array{T,2}
+   successors :: Array{T,2}
    sources    :: StateSpaceModel
 
-   function Catalog( data :: Vector{Array{T,1}}, ssm :: StateSpaceModel) where T
+   function Catalog( data :: Array{T,2}, ssm :: StateSpaceModel) where T
 
-       nt, nv     = length(data), length(data[1])
-       analogs    = @view data[1:end-ssm.dt_states]
-       successors = @view data[ssm.dt_states+1:end]
+       nv, nt     = size(data)
+       analogs    = @view data[ :, 1:end-ssm.dt_states]
+       successors = @view data[ :, ssm.dt_states+1:end]
 
        new{T}( nt, nv, data, analogs, successors, ssm)
 
    end
 
 end
-
