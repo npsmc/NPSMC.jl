c29f650575455e6f4b3df4943b33dbb753108e59
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-28 11:50:13 +0200

    Update state_space.jl

--- src/state_space.jl
+++ src/state_space.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 abstract type AbstractForecasting end
 
 export StateSpaceModel
@@ -37,4 +39,62 @@ struct StateSpaceModel <: AbstractForecasting
     sigma2_obs     :: Float64
     
 end
+
+
+"""
+    Generate simulated data from Space State Model
+     
+- var_obs            : indices of the observed variables
+- dy                 : dimension of the observations
+- Q                  : model covariance
+- R                  : observation covariance
+- dx                 : dimension of the state
+- dt_int             : fixed integration time
+- dt_model           : chosen number of model time step 
+- var_obs            : indices of the observed variables
+- dy                 : dimension of the observations
+- H                  : first and third variables are observed
+- h                  : observation model
+- jacH               : Jacobian of the observation model(for EKS_EM only)
+- Q                  : model covariance
+- R                  : observation covariance
+
+"""
+struct SSM
+
+     h        :: Function
+     jac_h    :: Function 
+     mx       :: Function
+     jac_mx   :: Function 
+
+     dt_int   :: Float64
+     dt_model :: Int64
+     dx       :: Int64
+     x0       :: Vector{Float64}
+     dy       :: Int64
+     var_obs  :: Vector{Int64}
+     sig2_Q   :: Float64 
+     sig2_R   :: Float64
+     Q        :: Symmetric
+     R        :: Symmetric
+
+
+     function SSM( h  :: Function, jac_h  :: Function,
+                   mx :: Function, jac_mx :: Function, 
+                   dt_int :: Float64, dt_model :: Int64, 
+                   x0 :: Vector{Float64}, var_obs :: Vector{Int64}, 
+                   sig2_Q :: Float64, sig2_R :: Float64)
+
+        dx       = length(x0)
+        dy       = length(var_obs)
+        Q        = Symmetric(Matrix(I,dx,dx) .* sig2_Q)
+        R        = Symmetric(Matrix(I,dx,dx) .* sig2_R)
+
+        new( h, jac_h, mx, jac_mx, dt_int, dt_model, dx,
+             x0, dy, var_obs, sig2_Q, sig2_R, Q, R)
+
+     end
+
+end
+
 
