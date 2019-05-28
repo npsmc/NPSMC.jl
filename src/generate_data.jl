2ae2c049d90c64e140551686d994c760b1431700
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-05-28 11:50:19 +0200

    Update generate_data.jl

--- src/generate_data.jl
+++ src/generate_data.jl
@@ -70,3 +70,59 @@ function generate_data( ssm :: StateSpaceModel,
     xt, yo, Catalog( hcat(catalog_tmp...), ssm )
 
 end
+
+""" 
+     generate_data(ssm, T_burnin, T; seed = 1)
+
+Generate simulated data from Space State Model
+"""
+function generate_data(ssm :: SSM, x0 :: Vector{Float64}, nt :: Int64; seed = 1)
+
+    Random.seed!(seed)
+
+    nv = length(x0)
+    μ = zeros(Float64, nv)
+    d = MvNormal(μ, ssm.Q)
+    # generate true state
+    xt = [zeros(Float64, nv) for i in 1:nt]
+    xt[1] .= x0
+    dt = ssm.dt_model * ssm.dt_int 
+    time = Float64[0.0]
+    for t in 1:nt-1
+        xtmp = xt[t]
+        for i in 1:ssm.dt_model
+            xtmp = ssm.mx(xtmp)
+        end
+        xt[t+1] .= xtmp .+ vec(rand(d, nv))
+        push!(time, t * dt)
+    end
+
+    # generate  partial/noisy observations
+    yo = xt .* NaN
+
+    d = MvNormal(μ, ssm.R)
+    for t in 1:nt-1
+        yo[t] .= vec(ssm.h(xt[t+1]) .+ rand(d, nv))
+    end
+
+
+    TimeSeries(time, xt), TimeSeries(time[1:end-1], yo)
+
+end
+    
+function train_test_split( X :: TimeSeries, Y :: TimeSeries; test_size=0.5)
+
+    time    = X.t
+    T       = length(time)
+    T_test  = Int64(T * test_size)
+    T_train = T - T_test
+
+    X_train = TimeSeries( time[1:T_train], X.u[:, 1:T_train-1])
+    Y_train = TimeSeries( time[2:end], Y.u[:, 1:T_train-1])
+
+    X_test = TimeSeries(time,     X.u[:, T_train:end])
+    Y_test = TimeSeries(time[2:end], Y.u[:, T_train:end-1])
+
+    X_train, Y_train, X_test, Y_test
+
+end
