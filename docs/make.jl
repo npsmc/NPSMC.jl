ffb5f1113e04f22fe4e1b342f3f5b5b164f495b0
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-22 18:28:20 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,27 +7,30 @@ using Plots
 # generate examples
 OUTPUT = joinpath(@__DIR__, "src", "generated")
 
-pages    = Any["Home" => "index.md",
-               "Models" => "models.md",
-               "Data Assimilation" => "data_assimilation.md",
-               "State Space" => "state_space.md"]
-
 examples = filter(x -> occursin(r".jl", x), map(relpath, readdir(joinpath(@__DIR__, "src", "examples"))))
+example_pages = Any[]
 
 for example in examples
+
 
     @show EXAMPLE = joinpath(@__DIR__, "src", "examples", example)
     @show page = string("generated/", example[1:end-3],".md")
 
     open(`head -n1 $EXAMPLE`) do io
          title = join(readdlm(io)[3:end], " ")
-         push!(pages, hide(title => page))
+         push!(example_pages, title => page)
     end
 
     Literate.markdown(EXAMPLE, OUTPUT)
     #Literate.notebook(EXAMPLE, OUTPUT)
     #Literate.script(EXAMPLE, OUTPUT)
 end
+
+pages = Any["Home" => "index.md",
+            "Models" => "models.md",
+            "Data Assimilation" => "data_assimilation.md",
+            "State Space" => "state_space.md",
+            "Examples" => example_pages ]
 
 makedocs(
     modules   = [NPSMC],
