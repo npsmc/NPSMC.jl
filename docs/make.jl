ce644040d5ae1adc97430a4ef3f1c9c373e1835f
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-24 20:03:53 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,27 +10,27 @@ OUTPUT = joinpath(@__DIR__, "src", "generated")
 examples = filter(x -> occursin(r".jl", x), map(relpath, readdir(joinpath(@__DIR__, "src", "examples"))))
 example_pages = Any[]
 
-for example in examples
-
-
-    @show EXAMPLE = joinpath(@__DIR__, "src", "examples", example)
-    @show page = string("generated/", example[1:end-3],".md")
-
-    open(`head -n1 $EXAMPLE`) do io
-         title = join(readdlm(io)[3:end], " ")
-         push!(example_pages, title => page)
-    end
-
-    Literate.markdown(EXAMPLE, OUTPUT)
-    #Literate.notebook(EXAMPLE, OUTPUT)
-    #Literate.script(EXAMPLE, OUTPUT)
-end
+#for example in examples
+#
+#
+#    @show EXAMPLE = joinpath(@__DIR__, "src", "examples", example)
+#    @show page = string("generated/", example[1:end-3],".md")
+#
+#    open(`head -n1 $EXAMPLE`) do io
+#         title = join(readdlm(io)[3:end], " ")
+#         push!(example_pages, title => page)
+#    end
+#
+#    Literate.markdown(EXAMPLE, OUTPUT)
+#    #Literate.notebook(EXAMPLE, OUTPUT)
+#    #Literate.script(EXAMPLE, OUTPUT)
+#end
 
 pages = Any["Home" => "index.md",
             "Models" => "models.md",
             "Data Assimilation" => "data_assimilation.md",
-            "State Space" => "state_space.md",
-            "Examples" => example_pages ]
+            "State Space" => "state_space.md"]
+            #"Examples" => example_pages ]
 
 makedocs(
     modules   = [NPSMC],
