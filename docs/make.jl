287298f786c0a02c953dc75844363e61a5a54ed9
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-11-25 13:56:47 +0100

    move examples directory

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,20 @@
+using DifferentialEquations
 using DelimitedFiles
 using Documenter
 using Literate
-using NPSMC
 using Plots 
+using NPSMC
 
 # generate examples
 OUTPUT = joinpath(@__DIR__, "src", "generated")
 
 #examples = filter(x -> occursin(r".jl", x), map(relpath, readdir(joinpath(@__DIR__, "src", "examples"))))
 #example_pages = Any[]
-
+#
 #for example in examples
 #
-#
-#    @show EXAMPLE = joinpath(@__DIR__, "src", "examples", example)
-#    @show page = string("generated/", example[1:end-3],".md")
+#    EXAMPLE = joinpath(@__DIR__, "src", "examples", example)
+#    page = string("generated/", example[1:end-3],".md")
 #
 #    open(`head -n1 $EXAMPLE`) do io
 #         title = join(readdlm(io)[3:end], " ")
@@ -22,9 +22,11 @@ OUTPUT = joinpath(@__DIR__, "src", "generated")
 #    end
 #
 #    Literate.markdown(EXAMPLE, OUTPUT)
-#    #Literate.notebook(EXAMPLE, OUTPUT)
-#    #Literate.script(EXAMPLE, OUTPUT)
+##    #Literate.notebook(EXAMPLE, OUTPUT)
+##    #Literate.script(EXAMPLE, OUTPUT)
 #end
+#
+#@show example_pages
 
 pages = Any["Home" => "index.md",
             "Catalog" => "catalog.md",
@@ -35,9 +37,11 @@ pages = Any["Home" => "index.md",
             "Ensemble Kalman smoothers" => "ensemble_kalman_smoothers.md",
             "Forecasting" => "forecasting.md",
             "Particle filters" => "particle_filters.md",
-            "Time Series" => "time-series.md"]
-#            "Utilities" => "utils.md"]
-#            #"Examples" => example_pages ]
+            "Time Series" => "time-series.md",
+            "Utilities" => "utils.md"]
+            #"Examples" => example_pages ]
+
+@show pages
 
 makedocs(
     modules   = [NPSMC],
@@ -45,10 +49,11 @@ makedocs(
     doctest   = true, 
     authors   = "Pierre Navaro",
     format    = Documenter.HTML(),
-    pages     = pages
+    pages     = pages,
+    debug     = true
 )
 
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/npsmc/NPSMC.jl.git",
- )
+)
